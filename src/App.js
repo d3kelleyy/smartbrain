b51dbc53ba61ce00b25b3d8cafe2de69bdff6f27
unknown <d3kelley@gmail.com>
2020-10-12 16:13:05 -0400

    updated onButtonSubmit method to respond with user entries

--- src/App.js
+++ src/App.js
@@ -84,9 +84,22 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) =>
+      .then((response) => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+        }
         this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      })
       .catch((err) => console.log(err));
   };
 
@@ -111,7 +124,7 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank name={this.state.user.name} entrie={this.state.user.entries}/>
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
