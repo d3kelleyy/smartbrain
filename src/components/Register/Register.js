ade8e03a20d668bb0e5514e4154d7be96ebd8ff8
unknown <d3kelley@gmail.com>
2020-10-14 21:31:20 -0400

    edited the fetch within onSubmitRegister()

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitRegister = () => {
-    fetch("http://localhost:3001/register", {
+    fetch("http://localhost:3000/register", {
       method: "post",
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
